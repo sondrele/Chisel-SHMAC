ebdad48f04a464fd41022d9f0ca9ba8e549e5a0a
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-28 14:37:29 +0100

    Make InputPort change the destination-coordinates for the packet depending on whether it is a reply or not

--- src/main/scala/router/InputPort.scala
+++ src/main/scala/router/InputPort.scala
@@ -6,12 +6,18 @@ class InputPort(n: Int) extends Module {
   val io = new Bundle {
     val fifo = new DecoupledFifoIO(PacketData())
 
-    val xDest     = UInt(OUTPUT, width = 4)
-    val yDest     = UInt(OUTPUT, width = 4)
+    val xDest = UInt(OUTPUT, width = 4)
+    val yDest = UInt(OUTPUT, width = 4)
   }
 
   io.fifo <> Module(new Fifo(PacketData(), n)).io
 
-  io.xDest := io.fifo.out.bits.xDest
-  io.yDest := io.fifo.out.bits.yDest
+  val packet = io.fifo.out.bits
+  when (packet.isReply) {
+    io.xDest := packet.xSender
+    io.yDest := packet.ySender
+  }.otherwise {
+    io.xDest := packet.xDest
+    io.yDest := packet.yDest
+  }
 }
