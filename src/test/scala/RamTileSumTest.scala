eda1af15984f22c92d508a6c5b50af1a8bd111c6
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-29 16:23:44 +0100

    Verify that the output port is only requesting to send data during the cycles that there is expected to be a read-response packet in the east output port

--- src/test/scala/RamTileSumTest.scala
+++ src/test/scala/RamTileSumTest.scala
@@ -64,31 +64,38 @@ class RamTileSumTest(t: RamTile) extends Tester(t) {
   }
 
   var sum = 0
-  for (i <- 0 until 11) {
+  for (i <- 0 until 13) {
     // Issue read-requests for the 8 next cycles. Read the written
     // packets in sequential order, at addresses 0-7
     if (i < 8) {
       poke(t.io.ports(0).inRequest, 1)
       poke(t.io.ports(0).inData, readPacket(i).litValue())
-      poke(t.io.ports(0).outReady, 1)
+    } else {
+      poke(t.io.ports(0).inRequest, 0)
+      poke(t.io.ports(0).inData, 0)
     }
-
-    step(1)
 
     // It takes 2 cycles for the first read-request to arrive at the ram-tile,
     // and two cycles for the response to arrive back to the sender.
-    val j = i - 3
+    val j = i - 4
 
     // Four cycles later, the east output fifo should have contain a packet for
     // the first read request
     if (j >= 0 && j < 8) {
+      poke(t.io.ports(0).outReady, 1)
       expect(t.io.ports(0).outRequest, 1)
       // Check that the packets is the response with data 1-8 for addresses 0-7
       // and sum together the values when the expected packet is at the output port
       if (expect(t.io.ports(0).outData, responsePacket(j + 1, j).litValue())) {
         sum += j + 1
       }
+    } else {
+      // Check that there is no out-request during the cycles
+      // that we do not expect to read a packet
+      expect(t.io.ports(0).outRequest, 0)
     }
+
+    step(1)
   }
 
   expect(sum == 36, s"Should sum correctly $sum == 36")
