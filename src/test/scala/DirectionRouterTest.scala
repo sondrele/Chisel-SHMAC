31e6904fd8afb6456fdb714bbfc019473a63f31e
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-31 16:41:30 +0100

    Update DirectionRouterTest with better assignment of PacketBundle

--- src/test/scala/DirectionRouterTest.scala
+++ src/test/scala/DirectionRouterTest.scala
@@ -5,40 +5,26 @@ import router._
 
 class DirectionRouterTest(a: DirectionRouter) extends Tester(a) {
   // Act like this is input/output from/to North, but packet is going east
-  val packetFromNorthToEast = PacketData.create(
-    address = 10,
-    xDest = 2,
-    yDest = 1,
-    xSender = 1,
-    ySender = 0
-  ).litValue()
+  def emptyPacket = Array[BigInt](0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
-  val packetFromNorthToNorth = PacketData.create(
-    address = 10,
-    xDest = 1,
-    yDest = 0,
-    xSender = 1,
-    ySender = 0
-  ).litValue()
+  def packetFromNorthToEast = Array[BigInt](10, 0, 0, 0, 0, 0, 0, 1, 0, 2, 1)
+
+  def packetFromNorthToNorth = Array[BigInt](10, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0)
 
   // Packet comes in, no valid data in module
   poke(a.io.inRequest, 1)
-
-  // poke(a.io.inData, packetFromNorthToEast)
-  poke(a.io.inData.header.address, 10)
-  poke(a.io.inData.dest.x, 2)
-  poke(a.io.inData.dest.y, 1)
-  poke(a.io.inData.sender.x, 1)
-  poke(a.io.inData.sender.y, 0)
-
+  poke(a.io.inData, packetFromNorthToEast)
   poke(a.io.inRead, 0)
-  expect(a.io.crossbarIn.header.address, 0)
+
+  expect(a.io.crossbarIn, emptyPacket)
   expect(a.io.inReady, 1)
   expect(a.io.outRequest, 0)
-  expect(a.io.outData.header.address, 0)
+  expect(a.io.outData, emptyPacket)
+
   poke(a.io.outWrite, 0)
-  poke(a.io.crossbarOut.header.address, 0)
+  poke(a.io.crossbarOut, emptyPacket)
   poke(a.io.outReady, 0)
+
   expect(a.io.direction, 0)
   expect(a.io.isEmpty, 1)
   expect(a.io.isFull, 0)
@@ -46,35 +32,19 @@ class DirectionRouterTest(a: DirectionRouter) extends Tester(a) {
   step(1)
 
   poke(a.io.inRequest, 1)
-  // poke(a.io.inData, packetFromNorthToNorth)
-  poke(a.io.inData.header.address, 10)
-  poke(a.io.inData.dest.x, 1)
-  poke(a.io.inData.dest.y, 0)
-  poke(a.io.inData.sender.x, 1)
-  poke(a.io.inData.sender.y, 0)
-
+  poke(a.io.inData, packetFromNorthToNorth)
   poke(a.io.inRead, 1)
 
-  // expect(a.input.fifo.out.bits, packetFromNorthToEast)
-  expect(a.input.fifo.out.bits.header.address, 10)
-  expect(a.input.fifo.out.bits.dest.x, 2)
-  expect(a.input.fifo.out.bits.dest.y, 1)
-  expect(a.input.fifo.out.bits.sender.x, 1)
-  expect(a.input.fifo.out.bits.sender.y, 0)
-
-  // expect(a.io.crossbarIn, packetFromNorthToEast)
-  expect(a.io.crossbarIn.header.address, 10)
-  expect(a.io.crossbarIn.dest.x, 2)
-  expect(a.io.crossbarIn.dest.y, 1)
-  expect(a.io.crossbarIn.sender.x, 1)
-  expect(a.io.crossbarIn.sender.y, 0)
-
+  expect(a.input.fifo.out.bits, packetFromNorthToEast)
+  expect(a.io.crossbarIn, packetFromNorthToEast)
   expect(a.io.inReady, 1)
   expect(a.io.outRequest, 0)
   expect(a.io.outData.header.address, 0)
+
   poke(a.io.outWrite, 0)
   poke(a.io.crossbarOut.header.address, 0)
   poke(a.io.outReady, 0)
+
   expect(a.io.direction, East.litValue)
   expect(a.io.isEmpty, 0)
   expect(a.io.isFull, 0)
@@ -85,34 +55,16 @@ class DirectionRouterTest(a: DirectionRouter) extends Tester(a) {
   poke(a.io.inData.header.address, 0)
   poke(a.io.inRead, 1)
 
-
-  // expect(a.input.fifo.out.bits, packetFromNorthToNorth)
-  expect(a.input.fifo.out.bits.header.address, 10)
-  expect(a.input.fifo.out.bits.dest.x, 1)
-  expect(a.input.fifo.out.bits.dest.y, 0)
-  expect(a.input.fifo.out.bits.sender.x, 1)
-  expect(a.input.fifo.out.bits.sender.y, 0)
-
-  // expect(a.io.crossbarIn, packetFromNorthToNorth)
-  expect(a.io.crossbarIn.header.address, 10)
-  expect(a.io.crossbarIn.dest.x, 1)
-  expect(a.io.crossbarIn.dest.y, 0)
-  expect(a.io.crossbarIn.sender.x, 1)
-  expect(a.io.crossbarIn.sender.y, 0)
-
+  expect(a.input.fifo.out.bits, packetFromNorthToNorth)
+  expect(a.io.crossbarIn, packetFromNorthToNorth)
   expect(a.io.inReady, 1)
   expect(a.io.outRequest, 0)
   expect(a.io.outData.header.address, 0)
+
   poke(a.io.outWrite, 1)
-
-  // poke(a.io.crossbarOut, packetFromNorthToNorth)
-  poke(a.io.crossbarOut.header.address, 10)
-  poke(a.io.crossbarOut.dest.x, 1)
-  poke(a.io.crossbarOut.dest.y, 0)
-  poke(a.io.crossbarOut.sender.x, 1)
-  poke(a.io.crossbarOut.sender.y, 0)
-
+  poke(a.io.crossbarOut, packetFromNorthToNorth)
   poke(a.io.outReady, 1)
+
   expect(a.io.direction, North.litValue)
   expect(a.io.isEmpty, 0)
   expect(a.io.isFull, 0)
@@ -122,21 +74,16 @@ class DirectionRouterTest(a: DirectionRouter) extends Tester(a) {
   poke(a.io.inRequest, 0)
   poke(a.io.inData.header.address, 0)
   poke(a.io.inRead, 0)
-  expect(a.input.fifo.out.bits.header.address, 0)
-  expect(a.io.crossbarIn.header.address, 0)
+  expect(a.input.fifo.out.bits, emptyPacket)
+  expect(a.io.crossbarIn, emptyPacket)
   expect(a.io.inReady, 1)
   expect(a.io.outRequest, 1)
-
-  // expect(a.io.outData, packetFromNorthToNorth)
-  expect(a.io.outData.header.address, 10)
-  expect(a.io.outData.dest.x, 1)
-  expect(a.io.outData.dest.y, 0)
-  expect(a.io.outData.sender.x, 1)
-  expect(a.io.outData.sender.y, 0)
+  expect(a.io.outData, packetFromNorthToNorth)
 
   poke(a.io.outWrite, 0)
-  poke(a.io.crossbarOut.header.address, 0)
+  poke(a.io.crossbarOut, emptyPacket)
   poke(a.io.outReady, 0)
+
   expect(a.io.direction, 0)
   expect(a.io.isEmpty, 1)
   expect(a.io.isFull, 0)
