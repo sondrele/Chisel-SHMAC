aff4b7bd5295f40ba242cfbe8b1c8200b12ef4af
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-11-11 11:27:46 +0100

    Rearranged signals

--- src/main/scala/sodor/rv32_3stage/top_shmac.scala
+++ src/main/scala/sodor/rv32_3stage/top_shmac.scala
@@ -23,16 +23,17 @@ class ShmacUnit extends Module
 
    val core   = Module(new Core(resetSignal = io.host.reset))
    val arbiter = Module(new SodorMemArbiter())
+
    core.io.imem <> arbiter.io.imem
    core.io.dmem <> arbiter.io.dmem
-   arbiter.io.mem <> io.mem
 
    core.io.host <> io.host
-
    // Connect wires that does not get connected with <>
    io.host.mem_req.ready := core.io.host.mem_req.ready
    io.host.mem_rep.valid := core.io.host.mem_rep.valid
    io.host.mem_rep.bits := core.io.host.mem_rep.bits
+
+   arbiter.io.mem <> io.mem
    arbiter.io.imem.req.bits.data := core.io.imem.req.bits.data
    arbiter.io.dmem.resp.ready := core.io.dmem.resp.ready
 }
