c145c35d9d39e35960d2b1a7ba9e1518aa7a6d64
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-11-11 11:28:22 +0100

    Fix indentation

--- src/main/scala/sodor/rv32_3stage/top_shmac.scala
+++ src/main/scala/sodor/rv32_3stage/top_shmac.scala
@@ -11,29 +11,29 @@ import scala.collection.mutable.HashMap
 
 class ShmacIo(implicit val conf: SodorConfiguration) extends Bundle
 {
-   val host = new HTIFIO()
-   val mem  = new MemPortIo(conf.xprlen)
+  val host = new HTIFIO()
+  val mem  = new MemPortIo(conf.xprlen)
 }
 
 class ShmacUnit extends Module
 {
-   implicit val sodor_conf = SodorConfiguration()
+  implicit val sodor_conf = SodorConfiguration()
 
-   val io = new ShmacIo()
+  val io = new ShmacIo()
 
-   val core   = Module(new Core(resetSignal = io.host.reset))
-   val arbiter = Module(new SodorMemArbiter())
+  val core   = Module(new Core(resetSignal = io.host.reset))
+  val arbiter = Module(new SodorMemArbiter())
 
-   core.io.imem <> arbiter.io.imem
-   core.io.dmem <> arbiter.io.dmem
+  core.io.imem <> arbiter.io.imem
+  core.io.dmem <> arbiter.io.dmem
 
-   core.io.host <> io.host
-   // Connect wires that does not get connected with <>
-   io.host.mem_req.ready := core.io.host.mem_req.ready
-   io.host.mem_rep.valid := core.io.host.mem_rep.valid
-   io.host.mem_rep.bits := core.io.host.mem_rep.bits
+  core.io.host <> io.host
+  // Connect wires that does not get connected with <>
+  io.host.mem_req.ready := core.io.host.mem_req.ready
+  io.host.mem_rep.valid := core.io.host.mem_rep.valid
+  io.host.mem_rep.bits := core.io.host.mem_rep.bits
 
-   arbiter.io.mem <> io.mem
-   arbiter.io.imem.req.bits.data := core.io.imem.req.bits.data
-   arbiter.io.dmem.resp.ready := core.io.dmem.resp.ready
+  arbiter.io.mem <> io.mem
+  arbiter.io.imem.req.bits.data := core.io.imem.req.bits.data
+  arbiter.io.dmem.resp.ready := core.io.dmem.resp.ready
 }
