d676a12f0747daa2cb2522428c115ef90b24e824
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-11-12 19:39:12 +0100

    Test sending the SW-instruction. No data being written yet

--- src/test/scala/ShmacDmemReqTest.scala
+++ src/test/scala/ShmacDmemReqTest.scala
@@ -35,13 +35,13 @@ class ShmacDmemReqTest(t: ShmacUnit) extends Tester(t) {
 
   step(1) // CYCLE 1
   //             Address        Width         rd            LD
-  val ld_a     = (0x123 << 20) | (0x2 << 12) | (0x1 << 7)  | 0x03
+  val ld_a     = (0x123 << 20) | (0x2 << 12) | (0x1 << 7)  | 0x03               // 0x12302083
   //             Address        Width         rd            LD
-  val ld_b     = (0x124 << 20) | (0x2 << 12) | (0x2 << 7)  | 0x03
+  val ld_b     = (0x124 << 20) | (0x2 << 12) | (0x2 << 7)  | 0x03               // 0x12402103
   //             Function       rs2           rs1           rd            ADD
-  val a_plus_b = (0x0 << 25)   | (0x2 << 20) | (0x1 << 15) | (0x3 << 7)  | 0x0
-  //             Function       rs2           Base          Function      Addr       SW
-  val sw_sum   = (0x0 << 25)   | (0x3 << 20) | (0x0 << 15) | (0x2 << 12) | (0xc << 7) | 0x23
+  val a_plus_b = (0x0 << 25)   | (0x2 << 20) | (0x1 << 15) | (0x3 << 7)  | 0x33 // 0x002081b3
+  //              rs2           Base          Function      Addr       SW
+  val sw_sum   = (0x3 << 20) | (0x0 << 15) | (0x2 << 12) | (0xc << 7) | 0x23    // 0x00302623
 
   poke(t.io.host.reset, 0)
 
@@ -122,6 +122,7 @@ class ShmacDmemReqTest(t: ShmacUnit) extends Tester(t) {
   step(1) // Cycle 5
   // Not requesting instruction
   expect(t.core.io.imem.req.valid, 0)
+  expect(t.core.io.imem.resp.ready, 0)
 
   // Requesting data
   expect(t.core.io.dmem.req.valid, 1)
@@ -141,11 +142,12 @@ class ShmacDmemReqTest(t: ShmacUnit) extends Tester(t) {
   peekArbiter()
   step(1) // Cycle 6
 
-  // Not requesting data
-  expect(t.core.io.dmem.req.valid, 0)
-
   // Requesting next instruction
   expect(t.core.io.imem.req.valid, 1)
+  expect(t.core.io.imem.resp.ready, 1)
+
+  // Not requesting data
+  expect(t.core.io.dmem.req.valid, 0)
   expect(t.io.mem.req.valid, 1)
   expect(t.io.mem.req.bits.addr, 0x2008) // PC not increased
   expect(t.io.mem.resp.ready, 1)
@@ -163,7 +165,7 @@ class ShmacDmemReqTest(t: ShmacUnit) extends Tester(t) {
   expect(t.core.io.dmem.req.bits.data, 0)
 
   peekArbiter()
-  step(1) // Cylce 7
+  step(1) // Cycle 7
 
   // Not requesting instruction
   expect(t.core.io.imem.req.valid, 0)
@@ -172,10 +174,52 @@ class ShmacDmemReqTest(t: ShmacUnit) extends Tester(t) {
   expect(t.core.io.dmem.req.valid, 0)
   expect(t.io.mem.req.valid, 0)
   expect(t.io.mem.req.bits.addr, 0x200c) // Next PC
-  peekArbiter()
-  
-  step(1) // Cylce 8
-  peekArbiter()
-  step(1) // Cylce 9
 
+  peekArbiter()
+  step(1) // Cycle 8
+
+  // Requesting next instruction
+  expect(t.core.io.imem.req.valid, 1)
+  expect(t.core.io.imem.resp.ready, 1)
+
+  // Not requesting data
+  expect(t.core.io.dmem.req.valid, 0)
+  expect(t.io.mem.req.valid, 1)
+  expect(t.io.mem.req.bits.addr, 0x200c) // Next PC
+  expect(t.io.mem.resp.ready, 1)
+
+  // Respond with next instruction
+  poke(t.io.mem.resp.valid, 1)
+  poke(t.io.mem.resp.bits.data, sw_sum)
+  expect(t.io.mem.resp.ready, 1)
+  expect(t.core.io.imem.resp.ready, 1)
+
+  // Verify that there is no dmem request
+  expect(t.core.io.dmem.resp.ready, 0) // Core ready to receive dmem request
+  expect(t.core.io.dmem.req.valid, 0) // Not requesting data
+//  expect(t.core.io.dmem.req.bits.addr, 0) // Is this signal random?
+  expect(t.core.io.dmem.req.bits.data, a_plus_b)
+
+  peekArbiter()
+  step(1) // Cycle 9
+
+  // Not requesting instruction
+  expect(t.core.io.imem.req.valid, 0)
+  expect(t.core.io.imem.resp.ready, 0)
+
+  // requesting data
+  expect(t.core.io.dmem.req.valid, 1)
+  expect(t.io.mem.req.valid, 1)
+  expect(t.io.mem.req.bits.addr, 0xc) // SW-address
+  peek(t.io.mem.req.bits.data)
+
+  // Stop sending instructions
+  poke(t.io.mem.resp.valid, 1)
+  poke(t.io.mem.resp.bits.data, 0)
+  expect(t.io.mem.resp.ready, 1)
+
+  peekArbiter()
+  step(1) // Cycle 10
+
+  peekArbiter()
 }
