a62798d4cfee4a44706bfd787bcbe9300485a12d
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-11-21 16:02:22 +0100

    Refactor out common test-function

--- src/test/scala/sodor/ShmacLoadAddStoreTest.scala
+++ src/test/scala/sodor/ShmacLoadAddStoreTest.scala
@@ -3,32 +3,7 @@ package sodor
 import Chisel._
 import Sodor.ShmacUnit
 
-class ShmacLoadAddStoreTest(t: ShmacUnit) extends Tester(t) {
-
-  def peekArbiter(): Unit = {
-    println("#-----")
-    println("# Printing arbiter signals")
-    peek(t.arbiter.io.mem)
-    peek(t.arbiter.io.imem.req.ready)
-    peek(t.arbiter.io.imem.req.valid)
-    peek(t.arbiter.io.imem.req.bits.addr)
-    //    peek(t.arbiter.io.imem.req.bits.data )
-    peek(t.arbiter.io.imem.req.bits.fcn)
-    peek(t.arbiter.io.imem.req.bits.typ)
-    peek(t.arbiter.io.imem.resp.ready)
-    peek(t.arbiter.io.imem.resp.valid)
-    peek(t.arbiter.io.imem.resp.bits.data)
-    peek(t.arbiter.io.dmem.req.ready)
-    peek(t.arbiter.io.dmem.req.valid)
-    peek(t.arbiter.io.dmem.req.bits.addr)
-    peek(t.arbiter.io.dmem.req.bits.data)
-    peek(t.arbiter.io.dmem.req.bits.fcn)
-    peek(t.arbiter.io.dmem.req.bits.typ)
-    peek(t.core.io.dmem.resp.ready)
-    peek(t.arbiter.io.dmem.resp.valid)
-    peek(t.arbiter.io.dmem.resp.bits.data)
-    println("#-----")
-  }
+class ShmacLoadAddStoreTest(t: ShmacUnit) extends ShmacTester(t) {
 
   poke(t.io.host.reset, 1)
   poke(t.io.mem.req.ready, 0)
---
+++ src/test/scala/sodor/ShmacTester.scala
@@ -0,0 +1,32 @@
+package sodor
+
+import Chisel._
+import Sodor.ShmacUnit
+
+class ShmacTester(t: ShmacUnit) extends Tester(t) {
+
+  def peekArbiter(): Unit = {
+    println("#-----")
+    println("# Printing arbiter signals")
+    peek(t.arbiter.io.mem)
+    peek(t.arbiter.io.imem.req.ready)
+    peek(t.arbiter.io.imem.req.valid)
+    peek(t.arbiter.io.imem.req.bits.addr)
+    peek(t.arbiter.io.imem.req.bits.fcn)
+    peek(t.arbiter.io.imem.req.bits.typ)
+    peek(t.arbiter.io.imem.resp.ready)
+    peek(t.arbiter.io.imem.resp.valid)
+    peek(t.arbiter.io.imem.resp.bits.data)
+    peek(t.arbiter.io.dmem.req.ready)
+    peek(t.arbiter.io.dmem.req.valid)
+    peek(t.arbiter.io.dmem.req.bits.addr)
+    peek(t.arbiter.io.dmem.req.bits.data)
+    peek(t.arbiter.io.dmem.req.bits.fcn)
+    peek(t.arbiter.io.dmem.req.bits.typ)
+    peek(t.core.io.dmem.resp.ready)
+    peek(t.arbiter.io.dmem.resp.valid)
+    peek(t.arbiter.io.dmem.resp.bits.data)
+    println("#-----")
+  }
+
+}
