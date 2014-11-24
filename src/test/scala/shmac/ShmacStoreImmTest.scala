f688c353ce1b9677d35b5853102a2c13d86b4f51
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-11-24 08:59:16 +0100

    Start testing integration between the tiles. Reading instructions working fine

--- src/test/scala/shmac/ShmacStoreImmTest.scala
+++ src/test/scala/shmac/ShmacStoreImmTest.scala
@@ -1,8 +1,9 @@
 import Chisel.Tester
-import main.scala.router.{East, North}
+import main.scala.router.{West, East, North}
 import main.scala.shmac.Shmac
 
 class ShmacStoreImmTest(s: Shmac) extends Tester(s) {
+  import s.proc.unit
 
   // Stop processor while memory is filled with instructions
   poke(s.io.host.reset, 1)
@@ -21,7 +22,21 @@ class ShmacStoreImmTest(s: Shmac) extends Tester(s) {
     0       // Dest y
   )
 
-  def readPacket(addr: Int) = Array[BigInt](
+  def ramResponse(instr: Int, addr: Int) = Array[BigInt](
+    addr,   // Header address
+    1,      // Header reply
+    0,      // Header writeReq
+    0,      // Header writeMask
+    0,      // Header exop
+    0,      // Header error
+    instr,  // Payload
+    0,      // Sender x
+    0,      // Sender y
+    1,      // Dest x
+    0       // Dest y
+  )
+
+  def ramRequest(addr: Int) = Array[BigInt](
     addr,   // Header address
     0,      // Header reply
     0,      // Header writeReq
@@ -29,11 +44,62 @@ class ShmacStoreImmTest(s: Shmac) extends Tester(s) {
     0,      // Header exop
     0,      // Header error
     0,      // Payload
-    2,      // Sender x
+    0,      // Sender x
     0,      // Sender y
     1,      // Dest x
     0       // Dest y
   )
+
+
+  def checkImemRequest(valid: Int,  addr: Int, imem_resp_ready: Int = 1): Unit = {
+    expect(unit.io.mem.req.valid, valid) // Gets valid after req.ready is set
+    expect(unit.io.mem.req.bits.fcn, 0)
+    expect(unit.io.mem.req.bits.typ, 7)
+    expect(unit.io.mem.req.bits.addr, addr)
+    expect(unit.io.mem.resp.ready, 1)
+    // Verify that the same signals is set for imem
+    expect(unit.core.io.imem.req.valid, valid)
+    expect(unit.core.io.imem.resp.ready, imem_resp_ready)
+    expect(unit.core.io.imem.req.bits.fcn, 0)
+    expect(unit.core.io.imem.req.bits.typ, 7)
+    expect(unit.core.io.imem.req.bits.addr, addr)
+    // Verify that there is no dmem request
+    expect(unit.core.io.dmem.resp.ready, 0)
+  }
+
+  def peekArbiter(): Unit = {
+    println("#-----")
+    println("# Printing arbiter signals")
+    peek(unit.arbiter.io.mem)
+    peek(unit.arbiter.io.imem.req.ready)
+    peek(unit.arbiter.io.imem.req.valid)
+    peek(unit.arbiter.io.imem.req.bits.addr)
+    peek(unit.arbiter.io.imem.req.bits.fcn)
+    peek(unit.arbiter.io.imem.req.bits.typ)
+    peek(unit.arbiter.io.imem.resp.ready)
+    peek(unit.arbiter.io.imem.resp.valid)
+    peek(unit.arbiter.io.imem.resp.bits.data)
+    peek(unit.arbiter.io.dmem.req.ready)
+    peek(unit.arbiter.io.dmem.req.valid)
+    peek(unit.arbiter.io.dmem.req.bits.addr)
+    peek(unit.arbiter.io.dmem.req.bits.data)
+    peek(unit.arbiter.io.dmem.req.bits.fcn)
+    peek(unit.arbiter.io.dmem.req.bits.typ)
+    peek(unit.core.io.dmem.resp.ready)
+    peek(unit.arbiter.io.dmem.resp.valid)
+    peek(unit.arbiter.io.dmem.resp.bits.data)
+    println("#-----")
+  }
+
+  def checkProcPort(port: Int, valid: Int, packet: Array[BigInt]): Unit = {
+    expect(s.proc.io.ports(port).out.valid, valid)
+    expect(s.proc.io.ports(port).out.bits, packet)
+  }
+
+  def checkRamPort(port: Int, valid: Int, packet: Array[BigInt]): Unit = {
+    expect(s.ram.io.ports(port).out.valid, valid)
+    expect(s.ram.io.ports(port).out.bits, packet)
+  }
 
   val emptyPacket = Array[BigInt](0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
@@ -49,25 +115,59 @@ class ShmacStoreImmTest(s: Shmac) extends Tester(s) {
   poke(s.ram.io.ports(East.index).in.valid, 1)
   poke(s.ram.io.ports(East.index).in.bits, writeInstruction(sw_a, 0x2004))
   step(1)
-  poke(s.ram.io.ports(East.index).in.valid, 1)
-  poke(s.ram.io.ports(East.index).in.bits, readPacket(0x2000))
-  step(1)
-  poke(s.ram.io.ports(East.index).in.valid, 1)
-  poke(s.ram.io.ports(East.index).in.bits, readPacket(0x2004))
-  step(1)
   poke(s.ram.io.ports(East.index).in.valid, 0)
   poke(s.ram.io.ports(East.index).in.bits, emptyPacket)
-  step(1)
-  step(1)
-  // Verify that the instructions were store correctly
-  poke(s.ram.io.ports(East.index).out.ready, 1)
-  expect(s.ram.io.ports(East.index).out.bits.payload, ld_a)
-  step(1)
-  expect(s.ram.io.ports(East.index).out.bits.payload, sw_a)
-
-
-  step(1) // 1
+  step(8) // 10
 
   // Start the processor, it will start fetching instructions
   poke(s.io.host.reset, 0)
+
+  checkImemRequest(1, 0x2000)
+
+  step(1)
+
+  checkImemRequest(0, 0x2004)
+
+  step(1)
+
+  // It takes two cycles to reach the Sodor-tiles output port
+  expect(s.proc.io.ports(East.index).out.valid, 1)
+  expect(s.proc.io.ports(East.index).out.bits, ramRequest(0x2000))
+
+  expect(s.ram.io.ports(West.index).in.ready, 1)
+  expect(s.ram.io.ports(West.index).in.bits, ramRequest(0x2000))
+
+  step(4)
+
+  // It takes four cycles from a packet is at the input port until the
+  // respective response is at the output port
+  expect(s.ram.io.ports(West.index).out.valid, 1)
+  expect(s.ram.io.ports(West.index).out.bits, ramResponse(ld_a, 0x2000))
+
+  expect(s.proc.io.ports(East.index).in.ready, 1)
+  expect(s.proc.io.ports(East.index).in.bits, ramResponse(ld_a, 0x2000))
+
+  step(3)
+
+  // Two cycles for the packet to reach the processor, it issues a request for
+  // the next instruction on the next cycle
+  checkImemRequest(1, 0x2004)
+
+  step(2)
+
+  expect(s.proc.io.ports(East.index).out.valid, 1)
+  expect(s.proc.io.ports(East.index).out.bits, ramRequest(0x2004))
+
+  expect(s.ram.io.ports(West.index).in.ready, 1)
+  expect(s.ram.io.ports(West.index).in.bits, ramRequest(0x2004))
+
+  step(4)
+
+  expect(s.ram.io.ports(West.index).out.valid, 1)
+  expect(s.ram.io.ports(West.index).out.bits, ramResponse(sw_a, 0x2004))
+
+  expect(s.proc.io.ports(East.index).in.ready, 1)
+  expect(s.proc.io.ports(East.index).in.bits, ramResponse(sw_a, 0x2004))
+
+  step(3)
 }
